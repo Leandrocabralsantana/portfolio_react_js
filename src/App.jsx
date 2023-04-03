f26bced00d01435f7ad5531047e0b53dde546a9e
Leandro Cabral Santana <100740901+Leandrocabralsantana@users.noreply.github.com>
2023-04-03 12:35:53 -0300

    Creating computer component
    Creating components/canvas/computer.jsx component with 3D modelling

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Stars
   return (
     <BrowserRouter>
     <div className ="relative z-0 bg-primary">
-      <div classNAme="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
         <Navbar />
         <Hero />
       </div>
