7fc0e746837c40e09068ab8037efa82ad425c796
Leandro Cabral Santana <100740901+Leandrocabralsantana@users.noreply.github.com>
2023-04-05 15:40:43 -0300

    changes in responsive
    mediaqueries fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
       </div>
       <ComputersCanvas />
 
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center top-[430px]">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.dev
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -22,8 +22,8 @@ const Computers = ({ isMobile }) => {
         
       <primitive 
         object={computer.scene}
-        scale={isMobile? 0.35 : 0.55}
-        position={isMobile? [-8, -4.0, -2.2] : [0,-4,-2.5]}
+        scale={isMobile? 0 : 0.55}
+        position={isMobile? 0: [0,-3,-2.5]}
         rotation={[-0.01, -0.2, -0.1]}
         />
     </mesh>
